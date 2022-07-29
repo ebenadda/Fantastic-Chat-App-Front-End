ac29f07e476751e1d8dcb6ec4cb86f75cd8aa24e
Sam <sandeshmas@gmail.com>
2022-07-28 19:26:32 -0700

    timestamp added

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ const form = document.getElementById("form");
 const handle = document.getElementById("handle");
 const message = document.getElementById("message");
 
+socket.on("message", (message) => {
+  outputMessage(message);
+});
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (handle.value && message.value) {
-    socket.emit("chat message", {
-      text: message.value,
-      sender: handle.value,
+    const msg = e.target.elements.handle.value;
+    const sender = e.target.elements.message.value;
+    // socket.emit("fromUser", msg);
+    socket.emit("fromUser", {
+      text: msg,
+      sender: sender,
     });
-    // handle.value = "";
+    e.target.elements.handle.value = " ";
+    e.target.elements.handle.focus();
   }
 });
 
-socket.on("chat message", function (msgObj) {
-  const item = document.createElement("li");
-  //Sender: text
-  const text = msgObj.text;
-  const sender = msgObj.sender;
-  item.textContent = `${text}: ${sender}`;
-  messages_container.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-});
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("newMessage");
+  div.innerHTML = `<p class="meta">${message.username} at <span>${message.time}</span></p>
+  <p class="text">${message.text}</p>`;
+  messages_container.appendChild(div);
+}
