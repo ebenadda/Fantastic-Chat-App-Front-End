f8c15972bcd9a5a32ed855424a11eeb32ba339c5
Sam <sandeshmas@gmail.com>
2022-07-29 11:08:33 -0700

    added heroku link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 
-const socket = io("http://localhost:3000/");
+const socket = io("https://fantastic-chat-app.herokuapp.com/");
 const messages_container = document.getElementById("messages-container");
 const form = document.getElementById("form");
 const onlineUsers = document.getElementById("online-user-container");
