f1fc531aba773c01d5ae66293ab2db00de8fab61
Sam <sandeshmas@gmail.com>
2022-07-28 20:23:57 -0700

    combining changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+
 const socket = io("http://localhost:3000/");
 const messages_container = document.getElementById("messages-container");
 const form = document.getElementById("form");
@@ -14,7 +15,7 @@ form.addEventListener("submit", function (e) {
   if (handle.value && message.value) {
     const msg = e.target.elements.handle.value;
     const sender = e.target.elements.message.value;
-    // socket.emit("fromUser", msg);
+
     socket.emit("fromUser", {
       text: msg,
       sender: sender,
